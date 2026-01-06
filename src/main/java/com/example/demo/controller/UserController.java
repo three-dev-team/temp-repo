b58988cace2f46793e58c094da36112521c40447
hssam99 <sumin5400@gmail.com>
2026-01-06 15:55:46 +0900

    feat: 회원가입 작업 완료

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
 
     @GetMapping("/login")
     public String login() {
@@ -25,6 +28,7 @@ public class UserController {
     @PostMapping("/signup")
     public String signupPost(UserDTO userDTO) {
         log.info(">>> ✅ 생성된 사용자 정보: " + userDTO);
+        String email = userService.insertUser(userDTO);
         return "redirect:/user/login";
     }
 }
