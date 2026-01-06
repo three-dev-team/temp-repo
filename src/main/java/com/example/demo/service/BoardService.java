cc4dd5d3325ce55d43cf48334eed66a9a96d1c1e
hyunseo2512 <nhs8421@gmail.com>
2026-01-06 12:05:22 +0900

    1. dto, entity, repo, serviceImpl

--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -1,11 +1,37 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.BoardDTO;
+import com.example.demo.entity.Board;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BoardService {
+public interface BoardService {
 
-    public String getBoard() {
-        return "Board Data";
+    default Board convertDtoToEntity(BoardDTO boardDTO){
+        return Board.builder()
+                .bno(boardDTO.getBno())
+                .title(boardDTO.getTitle())
+                .writer(boardDTO.getWriter())
+                .content(boardDTO.getContent())
+                .readCount(boardDTO.getReadCount())
+                .cmtQty(boardDTO.getCmtQty())
+                .fileQty(boardDTO.getFileQty())
+                .build();
     }
+
+    default BoardDTO convertEntityToDto(Board board){
+        return BoardDTO.builder()
+                .bno(board.getBno())
+                .title(board.getTitle())
+                .writer(board.getWriter())
+                .content(board.getContent())
+                .readCount(board.getReadCount())
+                .cmtQty(board.getCmtQty())
+                .fileQty(board.getFileQty())
+                .regDate(board.getRegDate())
+                .modDate(board.getModDate())
+                .build();
+    }
+
+
 }
